a438eed848b4dd5f44a4f25c6b42fec68ac69549
Yoga Meleniawan Pamungkas <yogameleniawan@its.ac.id>
2023-10-16 09:02:40 +0700

    fix: separate errors to folder

---
+++ errors/error.go
@@ -0,0 +1,74 @@
+package errors
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+
+	"github.com/yogameleniawan/golang-error-lines/logs"
+)
+
+func Error(message string) {
+	// get log file path
+	logFilePath := logs.WriteLogFile()
+
+	// Membuka file log untuk penulisan
+	logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalf("Gagal membuka file log: %v", err)
+	}
+	defer logFile.Close()
+
+	// Konfigurasi logger untuk menulis ke file
+	log.SetOutput(logFile)
+
+	logger := log.New(logFile, "Error: ", log.Ldate|log.Ltime|log.Lshortfile)
+	logger.Println(message)
+}
+
+func Info(message string) {
+	// get log file path
+	logFilePath := logs.WriteLogFile()
+
+	// Membuka file log untuk penulisan
+	logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalf("Gagal membuka file log: %v", err)
+	}
+	defer logFile.Close()
+
+	// Konfigurasi logger untuk menulis ke file
+	log.SetOutput(logFile)
+
+	logger := log.New(logFile, "Info: ", log.Ldate|log.Ltime|log.Lshortfile)
+	logger.Println(message)
+}
+
+func Warning(message string) {
+	// get log file path
+	logFilePath := logs.WriteLogFile()
+
+	// Membuka file log untuk penulisan
+	logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalf("Gagal membuka file log: %v", err)
+	}
+	defer logFile.Close()
+
+	// Konfigurasi logger untuk menulis ke file
+	log.SetOutput(logFile)
+
+	logger := log.New(logFile, "Warning: ", log.Ldate|log.Ltime|log.Lshortfile)
+	logger.Println(message)
+}
+
+func ErrorLines(err error) {
+	_, file, line, ok := runtime.Caller(1)
+
+	if ok {
+		fmt.Printf("Error: %s\nFile: %s\nLine: %d\n", err, file, line)
+	} else {
+		fmt.Printf("Error: %s\n", err)
+	}
+}
